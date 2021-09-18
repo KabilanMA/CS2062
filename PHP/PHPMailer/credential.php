8302ed5863847e7c8124451b6144a70fae787c56
Kabilan Mahathevan <84805141+KabilanMA@users.noreply.github.com>
2021-09-19 01:58:48 +0530

    PHPMailer files

---
+++ PHP/PHPMailer/credential.php
@@ -0,0 +1,8 @@
+<?php
+    define("EMAIL", "javatest190283@gmail.com");
+    define('PASS', 'cs2062php');
+    define('USER', 'root');
+    define('DB_PASS', 'Kv@25031999');
+    define('DB', 'hospitalManagement')
+
+?>
